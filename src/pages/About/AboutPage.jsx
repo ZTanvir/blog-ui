a6ba948a5e80ae225fc6c6092d06a5be85173159
zahirul <zahirulopel@gmail.com>
2025-12-19 16:04:36 +0600

    add about page

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -17,7 +17,7 @@ const BlogFeatures = [
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="space-y-6">
       <h1 className="text-center text-4xl font-bold">About Our Blog</h1>
       <div className="mt-4 mr-auto ml-auto h-1 w-20 bg-sky-500 sm:w-25"></div>
 
@@ -46,6 +46,38 @@ const AboutPage = () => {
             {feature.name}
           </div>
         ))}
+      </section>
+
+      <section className="rounded-lg px-4 py-8 shadow-lg">
+        <h2 className="text-xl font-bold">Technologies Used</h2>
+        <div className="mt-3 grid grid-cols-3">
+          <section>
+            <h3 className="font-lg font-semibold">Frontend</h3>
+            <div className="mt-2 space-y-1 text-sm">
+              <p>React</p>
+              <p>React Router</p>
+              <p>Tailwind CSS</p>
+              <p>Vite</p>
+            </div>
+          </section>
+          <section>
+            <h3 className="font-lg font-semibold">Backend</h3>
+            <div className="mt-2 space-y-1 text-sm">
+              <p>Nodejs</p>
+              <p>Express</p>
+              <p>PostgreSql</p>
+              <p>Prisma ORM</p>
+            </div>
+          </section>
+          <section>
+            <h3 className="font-lg font-semibold">Authentication</h3>
+            <div className="mt-2 space-y-1 text-sm">
+              <p>JWT</p>
+              <p>Bcrypt</p>
+              <p>Http-only cookies</p>
+            </div>
+          </section>
+        </div>
       </section>
     </div>
   );
