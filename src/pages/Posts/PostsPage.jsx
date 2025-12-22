8a45241b6a830fe8185d95c0017453440de31202
zahirul <zahirulopel@gmail.com>
2025-12-22 17:31:37 +0600

    add spinner when page is loading blog post

--- src/pages/Posts/PostsPage.jsx
+++ src/pages/Posts/PostsPage.jsx
@@ -1,12 +1,23 @@
 import useSWR from "swr";
 import postService from "../../services/postService";
 import PostCardsList from "../../components/PostCardsList";
+import Spinner from "../../components/Spinner";
 const PostsPage = () => {
   const { data, error, isLoading } = useSWR("api/posts", postService.getPosts);
 
   if (error) return <p>{error.message}</p>;
-  if (isLoading) return <p>Loading..</p>;
-
+  if (isLoading)
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner
+          isVisible={true}
+          width={80}
+          height={80}
+          color={"#3498db"}
+          secondaryColor={"#f3f3f3"}
+        />
+      </div>
+    );
   return (
     <div>
       <h1 className="text-center text-4xl font-bold text-gray-700">
