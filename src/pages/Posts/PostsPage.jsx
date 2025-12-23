593717b0c633e1362b5e36e3b0af1c7ae11fe53f
zahirul <zahirulopel@gmail.com>
2025-12-23 18:07:45 +0600

    filter post based on user search

--- src/pages/Posts/PostsPage.jsx
+++ src/pages/Posts/PostsPage.jsx
@@ -1,9 +1,17 @@
 import useSWR from "swr";
+import { useState } from "react";
 import postService from "../../services/postService";
 import PostCardsList from "../../components/PostCardsList";
 import Spinner from "../../components/Spinner";
 const PostsPage = () => {
   const { data, error, isLoading } = useSWR("api/posts", postService.getPosts);
+  const [searchPost, setSearchPost] = useState("");
+
+  const posts =
+    data &&
+    data.filter((post) =>
+      post.title.toLowerCase().includes(searchPost.toLowerCase()),
+    );
 
   if (error) return <p>{error.message}</p>;
   if (isLoading)
@@ -23,7 +31,18 @@ const PostsPage = () => {
       <h1 className="text-center text-4xl font-bold text-gray-700">
         Available <span className="text-sky-500">dev</span> blog articles:
       </h1>
-      {data && <PostCardsList posts={data} />}
+      <form>
+        <div className="flex flex-col lg:items-center">
+          <input
+            type="text"
+            value={searchPost}
+            onChange={(e) => setSearchPost(e.target.value)}
+            placeholder="Search by title.."
+            className="w-full max-w-sm rounded-sm border border-gray-200 px-2 py-2 font-light outline-gray-300"
+          />
+        </div>
+      </form>
+      {posts && <PostCardsList posts={posts} />}
     </div>
   );
 };
