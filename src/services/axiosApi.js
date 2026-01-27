45d28367980148a0d865a305162bf1c62b5a8352
zahirul <zahirulopel@gmail.com>
2026-01-27 17:03:55 +0600

    add new refresh token on every response with axios intercptor

--- src/services/axiosApi.js
+++ src/services/axiosApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getStoredAuthToken } from "../utils/authToken";
+import { getStoredAuthToken, setStoredAuthToken } from "../utils/authToken";
+import authServices from "./authServices";
 
 const axiosApi = axios.create({
   baseURL: "http://localhost:8000/",
@@ -12,7 +13,6 @@ const axiosApi = axios.create({
 // Attach token on refresh
 axiosApi.interceptors.request.use(function (config) {
   const token = getStoredAuthToken();
-  console.log("interceptor req,", token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -20,5 +20,32 @@ axiosApi.interceptors.request.use(function (config) {
 });
 
 // Refresh token after expire
+axiosApi.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("api/auth/refresh")
+    ) {
+      originalRequest._retry = true;
+      try {
+        const { accessToken: newToken } = await authServices.refreshToken(
+          "api/auth/refresh",
+          {},
+        );
+        setStoredAuthToken(newToken);
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return axiosApi(originalRequest);
+      } catch (err) {
+        console.error("Refresh token failed", err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default axiosApi;
