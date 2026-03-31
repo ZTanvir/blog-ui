2a35f87b1b8ceca6b3628b436ca8f93c55a3c652
zahirul <zahirulopel@gmail.com>
2026-03-31 13:35:55 +0600

    refactor commentList component and add delete btn

--- src/pages/Post/CommentList.jsx
+++ src/pages/Post/CommentList.jsx
@@ -3,6 +3,9 @@ import commentService from "../../services/commentService";
 import Spinner from "../../components/Spinner";
 import { LiaCommentDotsSolid } from "react-icons/lia";
 import { decode } from "html-entities";
+import { MdDelete } from "react-icons/md";
+import { UseUser } from "../../context/UserContext";
+import useSWRMutation from "swr/mutation";
 
 const CommentList = ({ postId }) => {
   const { data, error, isLoading } = useSWR(`api/comments/post/${postId}`, () =>
@@ -31,40 +34,75 @@ const CommentList = ({ postId }) => {
         <div>
           {data.length ? (
             data.map((c) => (
-              <article
-                className="mx-auto my-4 w-[90%] rounded bg-neutral-100/50 p-4 text-gray-600 shadow-sm sm:my-8 sm:p-6"
-                key={c.id}
-              >
-                <header className="text-sm">
-                  <p>
-                    By <strong>{c?.user?.username}</strong>
-                  </p>
-                  <p className="">
-                    <time>{new Date(c.createdAt).toDateString()}</time>
-                    <time className="ml-2">
-                      {new Date(c.createdAt).toLocaleTimeString()}
-                    </time>
-                  </p>
-                </header>
-                <div className="mt-2">
-                  <>
-                    <div>{decode(c.comment)}</div>
-                    {/* Renders as: Greetings <b>User</b> */}
-                  </>
-                </div>
-              </article>
+              <Comment key={c.id} postId={postId} commentData={c} />
             ))
           ) : (
-            <div className="mx-auto my-4 flex w-[90%] flex-col items-center justify-center rounded bg-neutral-100/50 p-4 shadow-sm sm:my-8 sm:p-6">
-              <LiaCommentDotsSolid size={70} color="gray" />
-              <p className="text-center text-gray-600">
-                No comments yet. Be the first to share your thoughts!
-              </p>
-            </div>
+            <EmptyComment />
           )}
         </div>
       )}
     </>
   );
 };
+
+function Comment({ postId, commentData }) {
+  const { user } = UseUser();
+  const { trigger, isMutating } = useSWRMutation(
+    `/api/comments/post/${postId}/comments/${commentData.id}`,
+    commentService.deleteComment,
+  );
+
+  const handleDeleteBtn = () => {
+    console.log("delete btn clicked");
+    trigger();
+  };
+
+  return (
+    <article
+      className="mx-auto my-4 w-[90%] rounded bg-neutral-100/50 p-4 text-gray-600 shadow-sm sm:my-8 sm:p-6"
+      key={commentData.id}
+    >
+      <header className="flex justify-between text-sm">
+        <div>
+          <p>
+            By <strong>{commentData?.user?.username}</strong>
+          </p>
+          <p className="">
+            <time>{new Date(commentData.createdAt).toDateString()}</time>
+            <time className="ml-2">
+              {new Date(commentData.createdAt).toLocaleTimeString()}
+            </time>
+          </p>
+        </div>
+        {user.id === commentData.userId && (
+          <button
+            disabled={isMutating}
+            onClick={handleDeleteBtn}
+            className="hover:cursor-pointer hover:opacity-80 disabled:opacity-80"
+          >
+            <MdDelete title="delete comment" size={25} color="#ff6467" />
+          </button>
+        )}
+      </header>
+      <div className="mt-2">
+        <>
+          <div>{decode(commentData.comment)}</div>
+          {/* Renders as: Greetings <b>User</b> */}
+        </>
+      </div>
+    </article>
+  );
+}
+
+function EmptyComment() {
+  return (
+    <div className="mx-auto my-4 flex w-[90%] flex-col items-center justify-center rounded bg-neutral-100/50 p-4 shadow-sm sm:my-8 sm:p-6">
+      <LiaCommentDotsSolid size={70} color="gray" />
+      <p className="text-center text-gray-600">
+        No comments yet. Be the first to share your thoughts!
+      </p>
+    </div>
+  );
+}
+
 export default CommentList;
--- src/services/commentService.js
+++ src/services/commentService.js
@@ -21,4 +21,15 @@ const addComment = async (url, { arg }) => {
   }
 };
 
-export default { getComments, addComment };
+const deleteComment = async (url, { arg }) => {
+  try {
+    const response = await axiosApi.delete(url, arg);
+    return response.data;
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data || "An unexpected error occurred";
+    throw errorMessage;
+  }
+};
+
+export default { getComments, addComment, deleteComment };
